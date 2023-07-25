007f96657384ffa39fc307c26e736fc905780bc9
sinyoung0110 <107450756+sinyoung0110@users.noreply.github.com>
2023-07-25 10:35:45 +0900

    [unrated] Title: 코드 처리하기, Time: 0.03 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String solution(String code) {
+        String ret="";
+        char mode = '0';
+        for(int idx=0; idx<code.length(); idx++){
+            if(code.charAt(idx)=='1'){
+                mode = mode == '0' ? '1' : '0';
+                continue;
+            }
+            if(mode=='0'){
+                if(idx%2==0)
+                    ret+=code.charAt(idx);
+            } 
+            else{
+                if(idx%2==1)
+                ret+=code.charAt(idx);
+        }
+            
+        }
+        if(ret.equals(""))
+            ret="EMPTY";
+        return ret;
+    }
+}
