bf952dc0c10b88e01b4c4501f8f7b87c0ac821c3
sinyoung0110 <107450756+sinyoung0110@users.noreply.github.com>
2023-08-06 00:04:38 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,12 +1,14 @@
 class Solution {
-    public int solution(int price) {
-        if(price >= 500000) {
-            price *= 0.8;
-        } else if(price >= 300000) {
-            price *= 0.9;
-        } else if(price >= 100000) {
-            price *= 0.95;
-        }
-        return price;
+    public double solution(int price) {
+        double answer = 0;
+        if(price>=500000)
+            answer=price-price*0.2;
+        else if(price>=300000)
+            answer=price-price*0.1;
+        else if(price>=100000)
+            answer=price-price*0.05;
+        else
+            answer=price;
+        return (int)answer;
     }
 }
