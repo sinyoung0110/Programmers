b709bafd381396604e323b170ad7c61553713c3f
sinyoung0110 <107450756+sinyoung0110@users.noreply.github.com>
2023-09-03 22:28:35 +0900

    [level 2] Title: 타겟 넘버, Time: 0.48 ms, Memory: 92.4 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/README.md
+++ 프로그래머스/lv2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 76.7 MB, 시간: 0.35 ms
+메모리: 92.4 MB, 시간: 0.48 ms
 
 ### 구분
 
--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -7,13 +7,12 @@ class Solution {
         return answer;
     }
 
-    // 깊이 우선 탐색
     public void dfs(int[] numbers, int depth, int target, int sum){
-        if(depth == numbers.length){ // 마지막 노드 까지 탐색한 경우
+        if(depth == numbers.length){
             if(target == sum) answer++;
         } else {
-            dfs(numbers, depth + 1, target, sum + numbers[depth]); // 해당 노드의 값을 더하고 다음 깊이 탐색
-            dfs(numbers, depth + 1, target, sum - numbers[depth]); // 해당 노드의 값을 빼고 다음 깊이 탐색
+            dfs(numbers, depth + 1, target, sum + numbers[depth]); 
+            dfs(numbers, depth + 1, target, sum - numbers[depth]);
         }
     }
 }
