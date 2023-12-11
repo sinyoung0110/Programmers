c79a73b63a255815fd13079b752bfa3dda631e84
sinyoung0110 <107450756+sinyoung0110@users.noreply.github.com>
2023-12-12 00:06:24 +0900

    [level unrated] Title: 최빈값 구하기, Time: 0.51 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120812. 최빈값 구하기/최빈값 구하기.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        int num=0;
+        Arrays.sort(array);
+        int max=array[array.length-1];
+        int[] count=new int[max+1];
+        for(int i=0; i<array.length; i++){
+            count[array[i]]++;
+        }
+        max=count[0];
+        for(int i=1; i<count.length; i++){
+            if(max<count[i]){
+                max=count[i];
+                answer=i;
+            }
+            else if(max==count[i])
+                answer=-1;
+        }
+        
+
+        return answer;
+    }
+}
