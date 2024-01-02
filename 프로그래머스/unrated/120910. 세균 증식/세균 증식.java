849f8c4d3b9062cb8bb63881aa8a0d4044aaffcc
sinyoung0110 <107450756+sinyoung0110@users.noreply.github.com>
2024-01-02 19:27:35 +0900

    [level unrated] Title: 세균 증식, Time: 0.03 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int n, int t) {
+        int answer = 1;
+        for(int i=0;i<t;i++){
+            n*=2;
+        }answer=n;
+        return answer;
+    }
+}
