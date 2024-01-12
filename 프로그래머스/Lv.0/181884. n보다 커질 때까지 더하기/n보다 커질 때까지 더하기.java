0fa8108a5d5317bb39fe2570c87b4c205ade026d
sinyoung0110 <107450756+sinyoung0110@users.noreply.github.com>
2024-01-12 22:27:13 +0900

    [level Lv.0] Title: n보다 커질 때까지 더하기, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] numbers, int n) {
+        int i = 0;
+        int sum=0;
+        
+        while(sum<=n){
+            sum+=numbers[i];
+            i++;
+        }
+        
+        return sum;
+    }
+}
