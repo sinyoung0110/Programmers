00ccfa41089849d1585c8cb3c775834781b1864f
sinyoung0110 <107450756+sinyoung0110@users.noreply.github.com>
2024-01-22 02:03:43 +0900

    [level 0] Title: 문자열 섞기, Time: 11.17 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String str1, String str2) {
+        String answer = "";
+        
+        for(int i=0; i<str1.length(); i++){
+            char a= str1.charAt(i);
+            char b= str2.charAt(i);
+            
+            answer+=""+a+b;
+        }
+        return answer;
+    }
+}
