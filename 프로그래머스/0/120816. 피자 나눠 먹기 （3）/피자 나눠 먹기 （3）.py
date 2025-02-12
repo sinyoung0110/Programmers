a085f739a0397fbc00e1cd58963656667c08f462
sinyoung0110 <107450756+sinyoung0110@users.noreply.github.com>
2025-02-12 15:50:22 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
@@ -1,8 +1,2 @@
 def solution(slice, n):
-    answer = 0
-    for i in range(0,100):
-        if slice*i>=n:
-            answer=i
-            break
-            
-    return answer
+    return ((n - 1) // slice) + 1
