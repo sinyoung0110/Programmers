6848e55502e54325551da13fbd4add15e3d2466a
sinyoung0110 <107450756+sinyoung0110@users.noreply.github.com>
2025-02-12 16:08:09 +0900

    [level 0] Title: 문자열 섞기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
@@ -1,6 +1,3 @@
 def solution(str1, str2):
-    answer=''
-    for i in range(0,len(str1)):
-        answer+=str1[i]+str2[i]
-    
+    answer = ''.join([str1[i] + str2[i] for i in range(len(str1))])
     return answer
