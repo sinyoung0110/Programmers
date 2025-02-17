e322f95166540dbdb8073f0928350b14f0a3b030
sinyoung0110 <107450756+sinyoung0110@users.noreply.github.com>
2025-02-17 14:28:33 +0900

    [level 0] Title: 코드 처리하기, Time: 1.06 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -1,16 +1,2 @@
 def solution(code):
-    answer = ''
-    mode=0
-    for idx in range(0,len(code)):
-        if code[idx]=="1":
-            mode=not mode 
-            continue
-        if mode==0:
-            if idx%2==0:
-                answer+=code[idx]
-        else:
-            if idx%2!=0:
-                answer+=code[idx]
-    if answer=="":
-        return "EMPTY"
-    return answer
+    return "".join(code.split("1"))[::2] or "EMPTY"
