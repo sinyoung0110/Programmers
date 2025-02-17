debcc3d192bb0678fb1fa731ac651fe20b18754d
sinyoung0110 <107450756+sinyoung0110@users.noreply.github.com>
2025-02-17 16:02:33 +0900

    [level 1] Title: 폰켓몬, Time: 1.04 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,7 +1,2 @@
 def solution(nums):
-    answer = 0
-    set_nums=set(nums)
-    if len(nums)/2 >= len(set_nums):
-        return len(set_nums)
-    
-    return len(nums)/2
+    return min(len(set(nums)), len(nums)//2)
