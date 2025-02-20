c3cdf6cce23dbbc53cfdcd7f002fc61f5b93dfac
sinyoung0110 <107450756+sinyoung0110@users.noreply.github.com>
2025-02-20 18:23:19 +0900

    [level 0] Title: 개미 군단, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.py
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.py
@@ -1,3 +1,6 @@
-
-def solution(hp):    
-    return hp // 5 + (hp % 5 // 3) + ((hp % 5) % 3)
+def solution(hp):
+    answer = 0
+    for ant in [5, 3, 1]:
+        d, hp = divmod(hp, ant)
+        answer += d
+    return answer
