3150b2aa5404e965e48d86b9cae84158efbc20aa
sinyoung0110 <107450756+sinyoung0110@users.noreply.github.com>
2025-02-20 18:18:51 +0900

    [level 0] Title: 개미 군단, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.py
@@ -0,0 +1,10 @@
+def solution(hp):
+    answer = 0
+    answer+=hp//5
+    if hp%5:
+        hp%=5
+        answer+=hp//3
+        if hp%3:
+            hp%=3
+            answer+=hp
+    return answer
